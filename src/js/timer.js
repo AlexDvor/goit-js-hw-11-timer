51ab69d919107361719e77c6e36e02fa426f954d
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-07-08 22:55:29 +0200

    finish

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,35 +1,34 @@
-
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.timerId = null;
     this.selector = selector;
     this.targetDate = targetDate;
-
+    
     this.start()
-      
-    };
     
+  };
+  
     start() {
-  this.timerId = setInterval(() => {
-    const time = this.targetDate - Date.now();
-    const { days, hours, mins, secs } = this.getRefs();
-
-    days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-    hours.textContent = this.pad(Math.floor(
-      (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    ));
-
-    mins.textContent = this.pad(Math.floor(
-      (time % (1000 * 60 * 60)) / (1000 * 60),
-    ));
-    
-    secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-  }, 1000);
+      this.timerId = setInterval(() => {
+        const time = this.targetDate - Date.now();
+       
+        const { days, hours, mins, secs } = this.getRefs();
+  
+        days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  
+        hours.textContent = this.pad(Math.floor(
+          (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        ));
+  
+        mins.textContent = this.pad(Math.floor(
+          (time % (1000 * 60 * 60)) / (1000 * 60),
+        ));
+        
+        secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+      }, 1000);
     }
-    
-
-    getRefs() {
+  
+  getRefs() {
     return {
       days: document.querySelector(
         `${this.selector} [data-value="days"]`,
@@ -42,19 +41,17 @@ class CountdownTimer {
       ),
       secs: document.querySelector(`${this.selector} [data-value="secs"]`),
     };
-    };
-    
+  };
 
-    pad(value) {
+  pad(value) {
     return String(value).padStart(2, '0');
-    };
-    
+  };
+
 };
 
-  
 
 const countdownTimer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Dec 24, 2021'),
+  targetDate: new Date('Dec 31, 2021'),
 
 });
